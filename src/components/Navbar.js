3b47e65ddd432819cd5d40c5c7e7b2aeb713e468
Caleb Lee <caleb.gwl@gmail.com>
2018-02-02 17:29:01 -0600

    Setup navbar links and pages

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Navbar extends Component {
   render() {
     return (
       <div className="navbar">
         <div className="navbar__content">
-          <img src="http://placecera.com/100/100" alt="" className="navbar__logo"/>
+          <Link to="/">
+            <img src="http://placecage.com/50/50" alt="" className="navbar__logo"/>
+          </Link>
           <ul className="navbar__links">
-            <li className="navbar__link">About</li>
-            <li className="navbar__link">Work</li>
-            <li className="navbar__link">Contact</li>
+            <li className="navbar__link"><Link to="/about">About</Link></li>
+            <li className="navbar__link"><Link to="/work">Work</Link></li>
+            <li className="navbar__link"><Link to="/contact">Contact</Link></li>
           </ul>
         </div>
       </div>
--- src/index.js
+++ src/index.js
@@ -2,5 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './reset.css';
 import App from './App';
+import Home from './components/Home';
+import About from './components/About';
+import Work from './components/Work';
+import Contact from './components/Contact';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import {
+  BrowserRouter as Router,
+  Route,
+} from 'react-router-dom'
+
+ReactDOM.render(
+  <Router>
+    <div>
+      <Route path="/" component={App} />
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/work" component={Work} />
+      <Route path="/contact" component={Contact} />
+    </div>
+  </Router>
+,
+document.getElementById('root'));
