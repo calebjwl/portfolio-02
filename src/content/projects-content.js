2ff9cefb766cd6060f29e274d59e7003961b9c34
Caleb Lee <caleb.gwl@gmail.com>
2018-02-08 19:34:41 -0600

    Import rough project content

--- src/content/projects-content.js
+++ src/content/projects-content.js
@@ -2,13 +2,63 @@ const projects = [
     {
         title: "Bella Divine",
         description: "A formal dress and gown boutique based in Huntsville, Alabama that needed an online presence with style. I worked closely with Bella Divine's management team to create a user-friendly website that encompasses their image and story.",
-        skills: ['HTML5', 'CSS3', 'Sass', 'EmberJS', 'Photoshop'],
+        skills: ['HTML5', 'CSS3', 'Sass', 'Ember.js', 'Photoshop'],
         screenshot: 'bella-divine.png',
         links: {
             github: 'https://github.com/calebjwl/',
             preview: 'https://belladivinedress.com'
         }
     },
+    {
+        title: "Tennessee Network Technologies",
+        description: 'I was hired to redesign the outdated website and logo for TNT. The company wanted a modern, sleek design with a more marketable visual representation of their company. This particular project helped me grow further in logo design, branding, and content development.',
+        skills: ['HTML5', 'CSS3', 'Sass', 'Ember.js', 'Photoshop'],
+        screenshot: 'tnt.png',
+        links: {
+            github: 'https://github.com/calebjwl/tnt-site',
+            preview: 'http://tnnettech.com'
+        }
+    },
+    {
+        title: "PracTip",
+        description: 'PracTip is an application I built with a partner in three weeks for our final project at the Iron Yard bootcamp. In the app, musicians can create an account, create forums and video posts, and challenge other users to complete their challenges and give feedback on posted videos. The app promotes a community of musicians helping one another get better at their instruments.',
+        skills: 'Ember | Adonis | HTML | CSS',
+        screenshot: 'practip.png',
+        links: {
+            github: 'https://github.com/practip',
+            preview: 'http://practip.surge.sh'
+        }
+    },
+    {
+        title: "JS Drum Machine",
+        description: 'Use your keyboard to play my personalized drum machine, complete with 808 drum sounds and custom backing tracks! I built this without any frameworks in an effort to better solidify my vanilla Javascript fluency with Wes Bos\' Javascript 30 series.',
+        skills: 'Javascript | HTML | CSS',
+        screenshot: 'drumkit.png',
+        links: {
+            github: 'https://github.com/calebjwl/01---JavaScript-Drum-Kit',
+            preview: 'http://calebsdrumkit.surge.sh/'
+        }
+    },
+    {
+        title: "MyContacts",
+        description: 'MyContacts is a CRUD (Create, Read, Update, Destroy) app I built using vanilla Javascript. Users can Create new contacts, Read the contacts as displayed in their contact list, as well as Update and Destroy their created contacts. Try it yourself!',
+        skills: 'Javascript | HTML | CSS',
+        screenshot: 'my-contacts.png',
+        links: {
+            github: 'https://github.com/calebjwl/15-contact-manager',
+            preview: 'http://my-contacts.surge.sh/'
+        }
+    },
+    {
+        title: "Wave Rider",
+        description: 'Wave Rider Surf Blog serves as a mockup design template for a business website and blog, featuring a banner, article body section, and interactive footer. I wanted to create a simple and clean layout that had a consistent color palette, highlighting the sunset tones of the banner image.',
+        skills: 'HTML | CSS',
+        screenshot: 'wave-rider.png',
+        links: {
+            github: 'https://github.com/calebjwl/06-surf-and-paddle',
+            preview: 'http://waverider.surge.sh'
+        }
+    }
 ]
 
 export default projects;
